864a600407d976b861b407d1f8439cfd314e8cd8
summereuna <summereuna@gmail.com>
2023-06-16 20:25:40 +0900

    Fix: just home page contents

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,14 @@
+import PageContent from "../components/PageContent";
 const HomePage = () => {
   return (
     <>
-      <h1>HomePage</h1>
+      <PageContent title="BTS 10th Anniversary FESTA!">
+        <h2>HAPPY 10th ANNIVERSARY, BTS & ARMY!</h2>
+        <p>
+          방탄소년단 데뷔 10주년을 기념하여 아미(ARMY)와 대중 모두가 즐길 수
+          있는 FESTA 소식을 빠르게 전해 드립니다!
+        </p>
+      </PageContent>
     </>
   );
 };
